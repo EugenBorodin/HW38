bb454a2b2db83fabcf7c6a9db957e9b9a8d97d01
Eugen Borodin <Azsalatay@gmail.com>
2023-03-12 19:26:27 +0100

    Add description

--- src/Pet.java
+++ src/Pet.java
@@ -12,4 +12,8 @@
 //Формат входных данных: количество строк с записями о домашних животных,
 // затем сами записи в описанном формате.
 public class Pet {
+  private Kind kind;
+  private String name;
+  private String birth;
+  public double weight;
 }
